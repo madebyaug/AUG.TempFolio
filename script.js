5e023b0a414cd48f433e4259fbaddf9b814a10c2
August Foreman <47118712+madebyaug@users.noreply.github.com>
2025-05-12 13:04:00 -0400

    the html js and css files

---
+++ script.js
@@ -0,0 +1,15 @@
+// On img hover,
+// replace span.ProjectTitle "text"
+// with TAG attribute text [Tag="text"]
+
+const imgHover = title;
+
+function getProjectTitle() {
+  if (imgHover == title) {
+    return `true`;
+  } else {
+    return `null`;
+  }
+}
+
+console.log(getProjectTitle);
